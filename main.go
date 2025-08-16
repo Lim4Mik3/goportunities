1eea5ca667e3521c840408ab7865c8227c022a47
Leonardo Oliveira - Lim4 Mik3 <lim4mik3@email.com>
2025-08-15 23:53:38 -0300

    feat: add router sub-package

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/Lim4Mik3/goportunities/router"
+
+func main() {
+
+	// Initialize router
+	router.Initialize()
+}
--- src/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Lim4Mik3/goportunities
-
-go 1.22.2
